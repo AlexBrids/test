fc3d44906e378018294f085de46b65c3111f81a0
Бридский Александр <bridskiy@JUPITER9.RU>
2022-02-24 15:50:37 +0300

    add task №9 last digit

---
+++ tasks9_last_of_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int GetLastDigit(int number)
+{
+    return number % 10;
+}
+
+int GetIntegerFormConsole(int min, int max)
+{
+    int result = 0;
+    bool isError = true;
+    while(isError)
+    {
+        Console.Write($"Введите целое число от {min} до {max}: ");
+        string input = Console.ReadLine();
+        result = int.Parse(input);
+        isError = result < min || result > max;      
+    }
+    return result;
+}
+
+int number = GetIntegerFormConsole(100, 999);
+int finish = GetLastDigit(number);
+Console.WriteLine(finish);
