a4920e85d504de68e1cee640ade725e32a46b453
Bridskiy Aleksandr <av-bridsky@yandex.ru>
2022-02-28 22:09:26 +0300

    add tasks 8 show even number

---
+++ tasks8_show_even_number/Program.cs
@@ -0,0 +1,23 @@
+﻿// показать четные числа от 1 до N
+
+Console.Clear();
+
+int GetNumber()
+{
+    int randomN = new Random().Next(2, 120);
+    return randomN;
+}
+
+int N = GetNumber();
+Console.WriteLine($"Случаное число равное: {N}");
+
+int number = 0;
+
+while (number < N)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number++;
+}
