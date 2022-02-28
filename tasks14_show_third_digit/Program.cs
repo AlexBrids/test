8a793d25f2645c3d340a189f64908a4b8da91f10
Bridskiy Aleksandr <av-bridsky@yandex.ru>
2022-02-28 22:27:07 +0300

    add tasks 14 show third digit

---
+++ tasks14_show_third_digit/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти третью цифру числа или сообщить что ее нет 
+
+Console.Clear();
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(50, 150);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine($"Исходное число: {number}");
+
+string str = Convert.ToString(number);
+int length = str.Length;
+//Console.WriteLine(length);
+
+if (length < 3)
+{
+    Console.WriteLine("Чило двузначное, третьего числа нет.");
+}
+else
+{
+    int result = number % 10;
+    Console.WriteLine($"Третье число, искомого числа: {result}");
+}
