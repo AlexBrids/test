c37390b56b4740efbd8e92017e243e5551307a9a
Bridskiy Aleksandr <av-bridsky@yandex.ru>
2022-02-28 21:42:18 +0300

    add task 12 delete second digit

---
+++ tasks12_delete_second_digit/Program.cs
@@ -0,0 +1,24 @@
+﻿// Удалить вторую цифру трехзначного числа 
+
+Console.Clear();
+
+int GetMumber ()
+{
+    int randomNumber = new Random().Next(100, 1000);
+    return randomNumber;
+}
+
+int number = GetMumber();
+Console.WriteLine($"Исходное число: {number}");
+
+void DeleteSecondDigit(int getNumber)
+{
+    int secondDigit = getNumber % 10;
+    string secondResult = Convert.ToString(secondDigit);
+    int firstDigit = getNumber / 100;
+    string firstResult = Convert.ToString(firstDigit);
+    Console.WriteLine(firstResult + secondResult);
+
+}
+
+DeleteSecondDigit(number);
